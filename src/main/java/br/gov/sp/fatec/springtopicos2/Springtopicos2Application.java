331ee5448980ee07ef4f98f5420adde4e265c6ee
ciscoquirino <quirino1988@gmail.com>
2024-08-13 10:28:07 -0300

    add files

---
+++ src/main/java/br/gov/sp/fatec/springtopicos2/Springtopicos2Application.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springtopicos2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springtopicos2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springtopicos2Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/gov/sp/fatec/springtopicos2/Springtopicos2ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.springtopicos2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springtopicos2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
